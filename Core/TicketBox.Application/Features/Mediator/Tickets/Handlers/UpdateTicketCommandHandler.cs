38c7e8f5344d8ff0f2254d1ffbad8faad7a2fb9a
Dilan <dilanderegizu@icloud.com>
2026-08-25 13:45:26 +0300

    updateticketcommand handler

--- Core/TicketBox.Application/Features/Mediator/Tickets/Handlers/UpdateTicketCommandHandler.cs
+++ Core/TicketBox.Application/Features/Mediator/Tickets/Handlers/UpdateTicketCommandHandler.cs
@@ -1,10 +1,4 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TicketBox.Application.Features.Mediator.Events.Commands;
 using TicketBox.Application.Features.Mediator.Tickets.Commands;
 using TicketBox.Persistence.Context;
 
